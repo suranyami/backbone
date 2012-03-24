6ada90e8481700ad9d3d44bbb954158acfbbf81b
David Parry <david.parry@suranyami.com>
2012-03-24 15:59:14 +1100

    Moved files to be more compatible with existing examples

---
+++ examples/websocket-todos/backbone-websockets.js
@@ -0,0 +1,83 @@
+Backbone.sync = function (method, model, options) {
+  var socket = window.socket;
+
+  /*
+   * Create signature object that will be emitted to server with every request. 
+   * This is used on the server to push an event back to the client listener.
+   */
+  var signature = function () {
+    var sig = {};    
+    sig.endPoint = model.url + (model.id ? ('/' + model.id) : '');
+    if (model.ctx) sig.ctx = model.ctx;
+    return sig;
+  };
+     
+  /*
+   * Create an event listener for server push. The server notifies
+   * the client upon success of CRUD operation.
+   */
+  var event = function (operation, sig) {
+    var e = operation + ':'; 
+    e += sig.endPoint;
+    if (sig.ctx) e += (':' + sig.ctx);
+ 
+    return e;
+  };
+     
+  // Save a new model to the server.
+  var create = function () {  
+    var sign = signature(model); 
+    var e = event('create', sign);
+    socket.emit('create', {'signature' : sign, item : model.attributes }); 
+    socket.once(e, function (data) {
+      model.id = data.id;  
+      console.log(model);                     
+    });                           
+  };              
+ 
+  // Get a collection or model from the server.
+  var read = function () {
+    var sign = signature(model);
+    var e = event('read', sign);
+    socket.send({name: 'read', 'signature' : sign});  
+    socket.once(e, function (data) {
+      options.success(data); // updates collection, model; fetch                      
+    });   
+  }; 
+     
+  // Save an existing model to the server.
+  var update = function () {
+    var sign = signature(model); 
+    var e = event('update', sign);
+    socket.emit('update', {'signature' : sign, item : model.attributes }); // model.attribues is the model data
+    socket.once(e, function (data) { 
+      console.log(data);                     
+    });                           
+  };  
+     
+  // Delete a model on the server.
+  var destroy = function () {
+    var sign = signature(model); 
+    var e = event('delete', sign);
+    socket.emit('delete', {'signature' : sign, item : model.attributes }); // model.attribues is the model data
+    socket.once(e, function (data) { 
+      console.log(data);                     
+    });                           
+  };             
+       
+  // entry point for method
+  switch (method) {
+    case 'create':
+      create();
+      break;
+    case 'read':  
+      read(); 
+      break;  
+    case 'update':
+      update();
+      break;
+    case 'delete':
+      destroy();
+      break; 
+  }        
+};
---
+++ examples/websocket-todos/server.coffee
@@ -0,0 +1,89 @@
+http = require("http")
+url = require("url")
+path = require("path")
+fs = require("fs")
+port = 8888
+
+http.createServer((request, response) ->
+  uri = url.parse(request.url).pathname
+  filename = path.join(process.cwd(), uri)
+  path.exists filename, (exists) ->
+    unless exists
+      response.writeHead 404,
+        "Content-Type": "text/plain"
+
+      response.write "404 Not Found\n"
+      response.end()
+      return
+    filename += "/index.html"  if fs.statSync(filename).isDirectory()
+    fs.readFile filename, "binary", (err, file) ->
+      if err
+        response.writeHead 500,
+          "Content-Type": "text/plain"
+
+        response.write err + "\n"
+        response.end()
+        return
+      response.writeHead 200
+      response.write file, "binary"
+      response.end()
+).listen parseInt(port, 10)
+
+console.log "Static file server running at\n  => http://localhost: #{port} /\nCTRL + C to shutdown"
+
+io = require("socket.io").listen(4000,
+  origins: "*:*"
+)
+
+io.set "origins", "*:*"
+io.set "log level", 1
+
+create = (socket, signature) ->
+  e = event("create", signature)
+  data = []
+  
+  socket.emit e,
+    id: 1
+  
+  console.log "created"
+
+read = (socket, signature) ->
+  e = event("read", signature)
+  data = undefined
+  data.push {}
+  socket.emit e, data
+  console.log "read"
+
+update = (socket, signature) ->
+  e = event("update", signature)
+  data = []
+  
+  socket.emit e,
+    success: true
+
+  console.log "update"
+
+destroy = (socket, signature) ->
+  e = event("delete", signature)
+  data = []
+  
+  socket.emit e,
+    success: true
+
+  console.log "destroy"
+
+event = (operation, sig) ->
+  "#{operation}: #{sig.endPoint}#{(":" + sig.ctx)  if sig.ctx}"
+
+io.sockets.on "connection", (socket) ->
+  socket.on "create", (data) ->
+    create socket, data.signature
+
+  socket.on "read", (data) ->
+    read socket, data.signature
+
+  socket.on "update", (data) ->
+    update socket, data.signature
+
+  socket.on "delete", (data) ->
+    destroy socket, data.signature
